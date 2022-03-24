1bc0e7da519fa1146177b0241d249e584757416d
Theo <theodore@tarr.com>
2022-03-24 17:32:33 -0400

    Changed buttons to blue and added a transition on hover

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -91,7 +91,7 @@ export const StatsModal = ({
           <div>
             <button
               type="button"
-              className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-center text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+              className="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-blue-700 px-4 py-2 text-center text-base font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:text-sm"
               onClick={() => {
                 shareStatus(
                   solution,
--- src/components/stats/ImmigratePanel.tsx
+++ src/components/stats/ImmigratePanel.tsx
@@ -95,8 +95,8 @@ export const ImmigratePanel = () => {
         disabled={!isSaveButtonEnabled}
         onClick={handleSaveButton}
         type="button"
-        className="mt-2 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-left text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:border-gray-200 
-          disabled:bg-white disabled:text-gray-900 disabled:focus:outline-none disabled:dark:border-gray-600 disabled:dark:bg-gray-800 disabled:dark:text-gray-400 sm:text-sm"
+        className="mt-2 inline-flex items-center justify-center rounded-md border border-transparent bg-blue-700 px-4 py-2 text-left text-base font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-blue-800 focus:outline-none focus:ring-2 
+          focus:ring-blue-500 focus:ring-offset-2 disabled:border-gray-200 disabled:bg-white disabled:text-gray-900 disabled:focus:outline-none disabled:dark:border-gray-600 disabled:dark:bg-gray-800 disabled:dark:text-gray-400 sm:text-sm"
       >
         {isSaveButtonEnabled && (
           <SaveIcon className="mr-2 h-6 w-6 cursor-pointer dark:stroke-white" />
